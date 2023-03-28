87bf538a67c9549bfb44676d2d0e98742c4d009d
Jonny de Abreu <97458860+Freynhel@users.noreply.github.com>
2023-03-28 15:42:36 -0300

    Added WhatGPT3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Footer, Blog, Possibility, Feature, WhatGPT3, Header } from './containers';
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
 import { CTA, Brand, Navbar } from './components';
 
 import './App.css';
@@ -14,7 +14,7 @@ const App = () => {
 			</div>
 			<Brand />
 			<WhatGPT3 />
-			<Feature />
+			<Features />
 			<Possibility />
 			<CTA />
 			<Blog />

60e1003a644fdec79c105a9d024b644a8febbf5f
Jonny de Abreu <97458860+Freynhel@users.noreply.github.com>
2023-04-13 01:01:45 -0300

    Added Possibility & CTA, Starting Blog

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
-	return (
-		<div>Article</div>
-	)
-}
+const Article = ({ imgUrl }) => (
+	<div className="gpt3__blog-container_article">
+		<div className="gpt3__blog-container_article-image">
+			<img src={imgUrl} alt="blog image" />
+		</div>
+		
+	</div>
+)
 
 export default Article
